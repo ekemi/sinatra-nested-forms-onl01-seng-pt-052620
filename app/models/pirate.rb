989b314e0cf5dcf04e99a4076afcdac68b02a35b
edmundo papi <edmundopierre@yahoo.com>
2020-09-25 00:23:29 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,13 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+   All = []
+  def initialize(name, weight, height)
+    @name = name
+    @weigth = weigth
+    @height = height
+    All << self
+  end
+  def self.all
+    All
+  end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+
+  attr_accessor :name, :type, :booty
+
+  All = []
+
+  def initialize(name, type, booty)
+    @name = name
+    @type = type
+    @booty = booty
+  end
+
+  def self.all
+    All
+
+  end
+
+  def self.clear
+    All.clear
+  end
 end
