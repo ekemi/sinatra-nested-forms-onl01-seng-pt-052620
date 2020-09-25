200877d24b90cb74ee0c6f43d55ab4db800f675b
edmundo papi <edmundopierre@yahoo.com>
2020-09-25 00:33:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -9,16 +9,19 @@ module FormsLab
       erb :root
     end
 
-  get '/new' do
-    erb :'pirates/new'
-  end
-  post "/pirates" do
-    @pirate = Pirate.new(params[:pirate])
-    params[:pirate][:ships].each do |details|
-      Ship.new(details)
+    get '/new' do
+      erb :'pirates/new'
     end
-    @ships = Ship.all
-    erb :'pirates/show'
+
+    post "/pirates" do
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+      @ships = Ship.all
+
+      erb :'pirates/show'
+    end
   end
-end
 end
